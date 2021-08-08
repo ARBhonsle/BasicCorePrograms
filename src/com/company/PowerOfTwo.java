2e9f2dbf6b15e9c1257f96b854a6745560f5c489
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-08 14:06:47 +0530

    Added Power of Two calculation table

---
+++ src/com/company/PowerOfTwo.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    static void powerTwo(int number){
+        int result=1;
+        for(int i=0;i<=number;i++){
+            System.out.println("2^"+(i)+" ="+result);
+            result=result*2;
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Give a positive number(N) [N>=0 && N<31]");
+        Scanner read =new Scanner(System.in);
+        int number=read.nextInt();
+        powerTwo(number);
+    }
+}
